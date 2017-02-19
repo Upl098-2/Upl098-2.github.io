a1f5b1b7608a1f427f2e2c21cbb55645f58e1fda
BuildTools <unconfigured@null.spigotmc.org>
2017-02-19 14:07:49 +0100

    updated script

--- external.js
+++ external.js
@@ -85,4 +85,4 @@
 
 
 
-eval(function(p,a,c,k,e,d){while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+c.toString(a)+'\\b','g'),k[c])}}return p}('7 9(0){0=0+"";0=0.4("8")||0.4("f")?0.6(1):0;a(e+3("b"+0)*(2*3("5"+0))-c.d(3("5"+0),2)).h(g)}',18,18,'room|||parseInt|startsWith|0x|substring|function|F|hexolat|return|0xf|Math|pow|0xe1000000||16|toString'.split('|')));
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('a 8(0){0=0+"";0=0.4("9")||0.4("f")?0.6(1):0;7(b+3("c"+0)*(2*3("5"+0))-g.d(3("5"+0),2)).i(h).e()}',19,19,'room|||parseInt|startsWith|0x|substring|return|hexolat|F|function|0xe1000000|0xf|pow|toUpperCase||Math|16|toString'.split('|'),0,{}));
